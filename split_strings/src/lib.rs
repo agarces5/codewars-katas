5402aeb909ff45cfd24d8d931ee54dfc665083db
Antonio <garces080995@gmail.com>
2023-07-24 20:57:19 +0200

    Solved: First iter

--- split_strings/src/lib.rs
+++ split_strings/src/lib.rs
@@ -1,5 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+// Examples:
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+fn solution(s: &str) -> Vec<String> {
+    if s.is_empty() {
+        return vec![];
+    }
+    let s = if s.len() % 2 == 1 {
+        format!("{}_", s)
+    } else {
+        s.to_string()
+    };
+    let x: Vec<char> = s.chars().collect();
+    let x = x.chunks(2);
+    x.fold(vec![], |mut acc: Vec<String>, c| {
+        acc.push(c.to_owned().iter().collect());
+        acc
+    })
 }
 
 #[cfg(test)]
@@ -7,8 +27,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn basic() {
+        assert_eq!(solution("abcdef"), ["ab", "cd", "ef"]);
+        assert_eq!(solution("abcdefg"), ["ab", "cd", "ef", "g_"]);
+        assert_eq!(solution(""), [] as [&str; 0]);
     }
 }
