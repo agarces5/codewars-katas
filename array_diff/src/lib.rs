ecd16fffffc7c4bd888cc92faeb92bd5bca1bedd
Antonio <garces080995@gmail.com>
2023-07-28 12:41:54 +0200

    First approach

--- array_diff/src/lib.rs
+++ array_diff/src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// array_diff(vec![1,2], vec![1]) == vec![2]
+
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// array_diff(vec![1,2,2,2,3], vec![2]) == vec![1,3]
+
+fn array_diff<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
+    let mut res = a;
+    b.iter().for_each(|b_n| res.retain(|a_x| a_x != b_n));
+    res
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn returns_expected() {
+        assert_eq!(array_diff(vec![1, 2], vec![1]), vec![2]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![1]), vec![2, 2]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![2]), vec![1]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![]), vec![1, 2, 2]);
+        assert_eq!(array_diff(vec![], vec![1, 2]), vec![]);
+        assert_eq!(array_diff(vec![1, 2, 3], vec![1, 2]), vec![3]);
     }
 }
