c4baa5fa26a1642aea44367ee5c4dec10da944e2
Antonio Garcés <garces080995@gmail.com>
2023-11-03 19:42:12 +0100

    Give me a diamond rustlike solution

--- give_me_a_diamond/src/lib.rs
+++ give_me_a_diamond/src/lib.rs
@@ -27,20 +27,15 @@
 // "  *\n ***\n*****\n ***\n  *\n"
 
 pub fn print(n: i32) -> Option<String> {
-    let mut buffer = "".to_string();
     match n % 2 {
         1 => {
-            for i in (1..=n).step_by(2) {
-                buffer += &" ".repeat(((n - i) / 2) as usize);
-                buffer += &"*".repeat(i as usize);
-                buffer += &"\n"
-            }
-            for i in (1..n).step_by(2) {
-                buffer += &" ".repeat(((i + 1) / 2) as usize);
-                buffer += &"*".repeat((n - 1 - i) as usize);
-                buffer += &"\n"
-            }
-            Some(buffer)
+            let n = n as usize;
+            let diamond = (1..=n)
+                .chain((1..n).rev())
+                .step_by(2)
+                .map(|i| format!("{}{}\n", " ".repeat((n - i) / 2), "*".repeat(i)))
+                .collect();
+            Some(diamond)
         }
         _ => None,
     }
